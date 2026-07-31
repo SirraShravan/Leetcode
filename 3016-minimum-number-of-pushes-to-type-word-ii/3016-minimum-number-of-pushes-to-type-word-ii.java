fcc4c28d82fdaa06117d9aa29f7f5f4d589d2ab1
SirraShravan <shravansirra12@gmail.com>
2026-07-31 20:59:32 +0530

    Time: 10 ms (66.76%), Space: 48.2 MB (17.34%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,31 +1,20 @@
 class Solution {
     public int minimumPushes(String word) {
-        HashMap<Character,Integer>map=new HashMap<>();
-        int n=word.length();
-           for(char ch:word.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        int[] freq = new int[26];
+        char[] chars = word.toCharArray();
+        for(char c : chars){
+            freq[c - 'a']++;
+        }
+        Arrays.sort(freq);
+        int cost = 0;
+        int assigned = 0;
 
-           }
-          List<Map.Entry<Character, Integer>> list =
-                new ArrayList<>(map.entrySet());
-               list.sort((a, b) -> b.getValue().compareTo(a.getValue()));
-               int count=0,size=1;
-               for(Map.Entry<Character, Integer> e : list){
-                if(size<=8){
-                    count+=e.getValue();
-                    
-                }
-                else if(size>8&&size<=16){
-                    count+=e.getValue()*2;
-                }
-                else if(size>16&&size<=24){
-                    count+=e.getValue()*3;
-                }
-                else{
-                    count+=e.getValue()*4;
-                }
-                size++;
-               }
-           return count;
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) break;
+            cost += freq[i] * (assigned / 8 + 1);
+            assigned++;
+        }
+
+        return cost;
     }
 }
