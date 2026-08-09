230ac13b26f6ffc1291f9279872d7ee47e709e97
SirraShravan <shravansirra12@gmail.com>
2026-08-09 18:02:46 +0530

    Time: 0 ms (100%), Space: 43.4 MB (5.76%) - LeetHub

---
+++ 2511-maximum-enemy-forts-that-can-be-captured/2511-maximum-enemy-forts-that-can-be-captured.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int captureForts(int[] forts) {
+       int ans=0;
+       int last=-1;
+       for(int i=0;i<forts.length;i++){
+        if(forts[i]==0){
+            continue;
+        }
+        if(last!=-1&&forts[last]!=forts[i]){
+            ans=Math.max(ans,i-last-1);
+        }
+        last=i;
+       }
+       return ans;
+    }
+}
