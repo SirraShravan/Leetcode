4175f2b030e540a43a7bff900156d369b0b014ce
SirraShravan <shravansirra12@gmail.com>
2026-08-11 12:41:41 +0530

    Time: 108 ms (13.61%), Space: 63.5 MB (11.78%) - LeetHub

---
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxSideLength(int[][] mat, int threshold) {
+        int m=mat.length;
+        int n=mat[0].length;
+        int[][] prefix=new int[m+1][n+1];
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+             prefix[i][j]=mat[i-1][j-1]+prefix[i-1][j]+prefix[i][j-1]-prefix[i-1][j-1];
+            }
+        }
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                for(int k=1;k+i<=m&&k+j<=n;k++){
+                    int sum=prefix[i+k][j+k]-prefix[i+k][j]-prefix[i][j+k]+prefix[i][j];
+                    if(sum<=threshold){
+                        ans=Math.max(ans,k);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
