9e76c4137c9d19c8559d1da268b824c45631f15e
SirraShravan <shravansirra12@gmail.com>
2026-08-12 19:16:00 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.48%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<3){
+            return n;
+        }
+        int x=1;
+        int y=2;
+        int z=0;
+        for(int i=3;i<=n;i++){
+          z=x+y;
+          x=y;
+          y=z;
+        }
+       return z;
+    }
+}
