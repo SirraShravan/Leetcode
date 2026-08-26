e9a70948182728c744aa29d9ef7afe01ecdc6de3
SirraShravan <shravansirra12@gmail.com>
2026-08-26 10:08:29 +0530

    Time: 3 ms (86.11%), Space: 43.2 MB (73.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>stack=new Stack<>();
+       int n=s.length();
+       for(int i=0;i<n;i++){
+        char ch=s.charAt(i);
+        if(ch=='['||ch=='{'||ch=='('){
+            stack.push(ch);
+        }else{
+            if(stack.isEmpty()){
+                return false;
+            }
+            char top=stack.pop();
+if(ch==']'&&top!='['){
+            return false;
+        }else if(ch=='}'&&top!='{'){
+            return false;
+        }else if(ch==')'&&top!='('){
+            return false;
+        }
+        } 
+       }
+       return stack.isEmpty();
+    }
+}
